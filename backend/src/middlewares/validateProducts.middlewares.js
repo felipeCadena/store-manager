c39f0256da0160c602df33653429d3c7e37c0d07
felipeCadena <felipecaden@gmail.com>
2023-11-21 20:51:58 -0300

    req 3 e 4 - conserta erro

--- backend/src/middlewares/validateProducts.middlewares.js
+++ backend/src/middlewares/validateProducts.middlewares.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
   const { name } = req.body;
-
   if (!name) res.status(400).json({ message: '"name" is required' });
   if (name.length < 5) {
     res.status(422).json({ 
